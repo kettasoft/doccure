743a1029e0b732e1cd5d675665cca3207f08b971
Abdalrhman Emad Saad <abdow.dev@gmail.com>
2022-04-08 15:52:26 +0200

    Add the Agent package to service provider

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Jenssegers\Agent\AgentServiceProvider::class,
 
         /*
          * Package Service Providers...
